617bed39661270feccdde463991b1c9d6f16db2b
PaulinaRog <rog.paulina@gmail.com>
2023-01-30 22:29:56 +0100

    landing page

---
+++ aquaristic/src/views/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div className="index">
+      <div className="index-layer"></div>
+      <div className="index-layer-2"></div>
+      <div className="index-layer-3"></div>
+      <div className="index-layer-4"></div>
+      <div className="index-layer-5"></div>
+    </div>
+  );
+}
