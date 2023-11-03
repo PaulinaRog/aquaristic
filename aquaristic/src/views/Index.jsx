af0f025bf53028a5c51d2be6c5f6a17180e388e4
PaulinaRog <rog.paulina@gmail.com>
2023-11-03 17:10:58 +0100

    fishes added

--- aquaristic/src/views/Index.jsx
+++ aquaristic/src/views/Index.jsx
@@ -1,4 +1,15 @@
 import React, { useState } from "react";
+import fishy from "../../src/assets/fishy.png";
+import turt from "../../src/assets/turtle.png";
+import fatfish from "../../src/assets/big-fat-fish.png";
+import coral from "../../src/assets/coral.png";
+import coral2 from "../../src/assets/coral2.png";
+import coral3 from "../../src/assets/coral3.png";
+import coral4 from "../../src/assets/coral4.png";
+import coral5 from "../../src/assets/coral5.png";
+import coral6 from "../../src/assets/coral6.png";
+import coral7 from "../../src/assets/coral7.png";
+import weeds from "../../src/assets/weeds.png";
 
 export default function Index() {
   const [fish, setFish] = useState(null);
@@ -31,33 +42,33 @@ export default function Index() {
 
   return (
     <div className="index">
-      <img src="../src/assets/coral.png" className="coral" />
+      <img src={coral} className="coral" />
       <div className="index-layer"></div>
-      <img src="../src/assets/coral2.png" className="coral2" />
+      <img src={coral2} className="coral2" />
       <div className="index-layer-2"></div>
-      <img src="../src/assets/coral3.png" className="coral3" />
-      <img src="../src/assets/coral4.png" className="coral4" />
-      <img src="../src/assets/coral5.png" className="coral5" />
-      <img src="../src/assets/coral6.png" className="coral6" />
-      <img src="../src/assets/coral7.png" className="coral7" />
-      <img src="../src/assets/weeds.png" className="weeds" />
+      <img src={coral3} className="coral3" />
+      <img src={coral4} className="coral4" />
+      <img src={coral5} className="coral5" />
+      <img src={coral6} className="coral6" />
+      <img src={coral7} className="coral7" />
+      <img src={weeds} className="weeds" />
       <div className="index-layer-3"></div>
       <div className="index-layer-4"></div>
       <div className="index-layer-5"></div>
       <img
-        src="../src/assets/fishy.png"
+        src={fishy}
         className="fishy"
         onMouseOver={handleHover}
         style={fish}
       />
       <img
-        src="../src/assets/turtle.png"
+        src={turt}
         className="turtle"
         onMouseOver={handleHoverTurtle}
         style={turtle}
       />
       <img
-        src="../src/assets/big-fat-fish.png"
+        src={fatfish}
         className="fatfish"
         onMouseOver={handleHoverFish}
         style={fish2}
